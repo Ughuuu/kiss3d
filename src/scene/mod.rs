d5a1104a9351789b8b7e39000f42b0c044baf358
Dragos Daian <daian.dragos@yahoo.com>
2026-09-21 08:58:11 +0200

    The whole-image rectangle is public beside the instance that takes it

--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -3,7 +3,7 @@
 pub use self::animation::{AnimationChannel, AnimationClip, AnimationPlayer, Interpolation};
 pub use self::object2d::{
     Blend2d, InstanceComputeBuffers2d, InstanceData2d, InstancesBuffer2d, Object2d, ObjectData2d,
-    LINES_COLOR_USE_OBJECT_2D, LINES_WIDTH_USE_OBJECT_2D, POINTS_COLOR_USE_OBJECT_2D,
+    LINES_COLOR_USE_OBJECT_2D, LINES_WIDTH_USE_OBJECT_2D, POINTS_COLOR_USE_OBJECT_2D, UV_WHOLE_2D,
     POINTS_SIZE_USE_OBJECT_2D,
 };
 pub use self::object3d::{
